870c482e7fa32f8cf6cf95c54f5b0c5c5feb3f9e
yamasaki junpei <opss0622@gmail.com>
2023-05-11 11:30:50 +0900

    feat: モーダル表示中の背景コンテンツをスクロールさせない機能を追加

--- src/redux/modalSlice.tsx
+++ src/redux/modalSlice.tsx
@@ -14,6 +14,11 @@ export const modalSlice = createSlice({
   reducers: {
     toggleShowModal: (state) => {
       state.modalIsOpen = !state.modalIsOpen;
+
+      // iOSの場合はモーダル表示中の背景コンテンツをスクロールさせないためにhtmlタグにもスタイルを適用する
+      if (navigator.userAgent.match(/iPhone/)) {
+        document.querySelector("html")?.classList.toggle("is-hidden");
+      }
     },
   },
 });
