01ad034078d6fa42851b758888162da19f9c2fda
yamasaki junpei <opss0622@gmail.com>
2023-05-11 19:56:01 +0900

    feat: Titleコンポーネントを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
+import ReactModal from "react-modal";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
 import { Board } from "./components/Board";
 import { Modal } from "./components/Modal";
-import ReactModal from "react-modal";
+import { Title } from "./components/Title";
 import "./styles.css";
 
 ReactModal.setAppElement("#root");
@@ -12,10 +13,13 @@ export default function App() {
     <Provider store={store}>
       <div className="App">
         <div className="container">
-          <Board category="TODO" />
-          <Board category="Doing" />
-          <Board category="Waiting" />
-          <Board category="Done" />
+          <Title />
+          <div className="boxContainer">
+            <Board category="TODO" />
+            <Board category="Doing" />
+            <Board category="Waiting" />
+            <Board category="Done" />
+          </div>
         </div>
         <Modal />
       </div>
---
+++ src/components/Title.tsx
@@ -0,0 +1,10 @@
+import "../css/title.css";
+
+export function Title() {
+  return (
+    <div className="titleContainer">
+      <h1 className="title">Creating basic Drag & Drop 👆</h1>
+      <span className="subtitle">( without libraries )</span>
+    </div>
+  );
+}
